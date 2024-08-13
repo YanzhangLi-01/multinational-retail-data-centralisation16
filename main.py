61b58485cbf054baf89fdb042e689505100eed24
YanzhangLi-01 <yanzhanglee@outlook.com>
2024-08-13 22:26:27 +0100

    Modified api problem

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     print("Card data cleaned and uploaded to local PostgreSQL successfully.")
 
     # Extract, clean, and upload store data
-    api_key = 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'
+    api_key = 'X'
     headers = {'x-api-key': api_key}
     number_of_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
     store_details_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
